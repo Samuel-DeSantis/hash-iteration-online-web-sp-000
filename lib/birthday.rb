595a3a76a931130d499493a229c7643834f79b2a
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-10-28 04:45:18 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,7 +9,7 @@ def happy_birthday(birthday_kids)
 end
 
 
-def happy_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kid, age|
     puts "Happy Birthday #{kid}! You are now #{age} years old!" if age <= 12
   end
